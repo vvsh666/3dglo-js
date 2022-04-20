4f32055f22853b22167cd7457a2cdd42726af864
Victor <vvsh1978@mail.ru>
2022-04-20 21:12:26 +0500

    feat: make lesson20-hard add scrollSmooth

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -3,16 +3,34 @@ const menu = () => {
     const menu = document.querySelector('menu')
     const closeBtn = menu.querySelector('.close-btn')
     const menuLinks = menu.querySelectorAll('ul>li>a')
+    const scrollUp = document.querySelector('main>a[href="#service-block"]')
 
     const handleMenu = () => {
         menu.classList.toggle('active-menu')
+    }
+
+    const scrollSmoothToAnchor = (link, event) => {
+        event.preventDefault()
+        let element = document.getElementById(link.getAttribute('href').substring(1))
+        element.scrollIntoView({behavior: 'smooth'})
     }
 
     menuBtn.addEventListener('click', handleMenu)
 
     closeBtn.addEventListener('click', handleMenu)
 
-    menuLinks.forEach(menuLink => menuLink.addEventListener('click', handleMenu))
+    menuLinks.forEach(menuLink => {        
+        menuLink.addEventListener('click', (event) => {
+            scrollSmoothToAnchor(menuLink, event)
+            handleMenu()
+        })        
+    })
+
+    scrollUp.addEventListener('click', (e) => {
+        scrollSmoothToAnchor(scrollUp, e)
+    })
+
+
 }
 
 export default menu
