d57b9cd6ef932f4c750f1455cfe51d290267a96e
svv <vvsh1978@mail.ru>
2022-04-22 11:26:58 +0500

    feat: make lesson21 hard

--- src/modules/validate.js
+++ src/modules/validate.js
@@ -6,31 +6,56 @@ const validate = () => {
     const userEmailInputs = document.querySelectorAll('input[name=user_email]')
     const userMessageInput = document.querySelector('input[name=user_message]')
 
+    const delSpaceHyphen = (e) => {
+        e.target.value = e.target.value.replace(/\s{2,}/g, ' ')
+        e.target.value = e.target.value.replace(/-{2,}/g, '-')
+        e.target.value = e.target.value.replace(/^[\s-]+|[\s-]+$/g, '')
+    }
+
+    const changeLetters = (e) => {
+        if (e.target.value != '') {
+            const arr = e.target.value.split(' ')
+            let resArr = arr.map(item => {
+                return item = item[0].toUpperCase() + item.slice(1).toLowerCase()
+            })
+            e.target.value = resArr.join(' ')
+        }
+    }
+
 
     userNameInputs.forEach((input) => {
         input.addEventListener('input', (e) => {
             e.target.value = e.target.value.replace(/[^А-Яа-яё -]+/, '')
         })
         input.addEventListener('blur', (e) => {
-            e.target.value = e.target.value.replace(/\s{2,}/g, ' ')
-            e.target.value = e.target.value.replace(/-{2,}/g, '-')
-            e.target.value = e.target.value.replace(/^\s+/, '').replace(/^-+/, '').replace(/^\s+/, '').replace(/^-+/, '')
-
+            delSpaceHyphen(e)
+            changeLetters(e)
         })
     })
     userPhoneInputs.forEach((input) => {
         input.addEventListener('input', (e) => {
             e.target.value = e.target.value.replace(/[^0-9\(\)-]+/, '')
         })
+        input.addEventListener('blur', (e) => {
+            delSpaceHyphen(e)
+        })
     })
     userEmailInputs.forEach((input) => {
         input.addEventListener('input', (e) => {
             e.target.value = e.target.value.replace(/[^\w@\-\_\.\!\~\*\']+|\^\\/, '')
         })
+        input.addEventListener('blur', (e) => {
+            delSpaceHyphen(e)
+        })
     })
     userMessageInput.addEventListener('input', (e) => {
         e.target.value = e.target.value.replace(/[^А-Яа-яё -]+/, '')
     })
+    userMessageInput.addEventListener('blur', (e) => {
+        delSpaceHyphen(e)
+        changeLetters(e)
+    })
+
 }
 
 export default validate
