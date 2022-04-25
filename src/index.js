c8264ccaa75ad9dddd0ec1bb754179773571e49a
svv <vvsh1978@mail.ru>
2022-04-25 20:34:39 +0500

    feat: make video tutorial lesson22

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from './modules/menu'
 import modal from './modules/modal'
 import calc from './modules/calc'
 import validate from './modules/validate'
+import tabs from './modules/tabs'
 
 timer('25 april 2022')
 menu()
 modal()
 calc()
 validate()
+tabs()
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,7 +1,6 @@
 const modal = () => {
     const buttons = document.querySelectorAll('.popup-btn')
     const modal = document.querySelector('.popup')
-    const closeBtn = modal.querySelector('.popup-close')
 
     buttons.forEach(btn => {
         btn.addEventListener('click', () => {
@@ -28,8 +27,10 @@ const modal = () => {
         })
     })
 
-    closeBtn.addEventListener('click', () => {
-        modal.style.display = 'none'
+    modal.addEventListener('click', (e) => {
+        if (!e.target.closest('.popup-content') || e.target.classList.contains('popup-close')) {
+            modal.style.display = 'none'
+        }
     })
 }
 
