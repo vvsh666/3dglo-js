2be4680fefcd7d01ecc37104e995acf7e1771188
svv <vvsh1978@mail.ru>
2022-04-26 16:16:53 +0500

    feat: make video tutorial lesson23

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import modal from './modules/modal'
 import calc from './modules/calc'
 import validate from './modules/validate'
 import tabs from './modules/tabs'
+import slider from './modules/slider'
 
 timer('25 april 2022')
 menu()
@@ -11,3 +12,4 @@ modal()
 calc()
 validate()
 tabs()
+slider()
