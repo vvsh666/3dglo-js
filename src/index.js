69a998b9168c1bdaaa0218606c6c73ae0f6e3b17
svv <vvsh1978@mail.ru>
2022-05-03 22:10:45 +0500

    feat: make video tutorial lesson27

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import calc from './modules/calc'
 import validate from './modules/validate'
 import tabs from './modules/tabs'
 import slider from './modules/slider'
+import sendForm from './modules/sendForm'
 
 timer('25 april 2022')
 menu()
@@ -13,3 +14,27 @@ calc(100)
 validate()
 tabs()
 slider('portfolio-content', 'portfolio-item', 'portfolio-dots', 'portfolio-btn')
+sendForm({
+    idForm: 'form1', someElem: [
+        {
+            type: 'block',
+            id: 'total'
+        }
+    ]
+})
+sendForm({
+    idForm: 'form2', someElem: [
+        {
+            type: 'block',
+            id: 'total'
+        }
+    ]
+})
+sendForm({
+    idForm: 'form3', someElem: [
+        {
+            type: 'block',
+            id: 'total'
+        }
+    ]
+})
---
+++ src/modules/sendForm.js
@@ -0,0 +1,80 @@
+'use strict'
+
+const sendForm = ({ idForm, someElem = [] }) => {
+    const form = document.getElementById(idForm)
+    const statusBlock = document.createElement('div')
+    const loadText = 'Загрузка...'
+    const errorText = 'Ошибка...'
+    const successText = 'Спасибо! Наш менеджер с вами свяжется!'
+
+    const validate = (list) => {
+        let success = true
+
+        return success
+    }
+
+    const sendData = (data) => {
+        return fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json())
+    }
+
+    const submitForm = () => {
+        const formElements = form.querySelectorAll('input')
+        const formData = new FormData(form)
+        const formBody = {}
+
+        statusBlock.textContent = loadText
+        form.append(statusBlock)
+
+        formData.forEach((val, key) => {
+            formBody[key] = val
+        })
+
+        someElem.forEach((elem) => {
+            const element = document.getElementById(elem.id)
+
+            if (elem.type === 'block') {
+                formBody[elem.id] = element.textContent
+            } else if (elem.type === 'input') {
+                formBody[elem.id] = element.value
+            }
+        })
+
+        if (validate(formElements)) {
+            sendData(formBody).then(data => {
+                statusBlock.textContent = successText
+
+                formElements.forEach(element => {
+                    element.value = ''
+                })
+            })
+                .catch(error => {
+                    statusBlock.textContent = errorText
+                })
+        } else {
+            alert('Данные не валидны!!!')
+        }
+    }
+
+    try {
+        if (!form) {
+            throw new Error('Верните на место форму, пожалуйста!')
+        }
+
+        form.addEventListener('submit', (e) => {
+            e.preventDefault()
+
+            submitForm()
+        })
+    } catch (error) {
+        console.log(error.message);
+    }
+
+}
+
+export default sendForm
